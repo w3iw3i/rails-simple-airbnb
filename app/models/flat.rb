406ce38e06f0533395b66f24f65f7889d236af62
Chia Wei Ng <chiawei2410@gmail.com>
2021-05-03 23:52:54 +0800

    rails simple airbnb

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,5 @@
 class Flat < ApplicationRecord
+  validates :name, :address, presence: :true
+  validates :number_of_guests, numericality: { only_integer: true, :greater_than_or_equal_to => 1 }
+  validates :price_per_night, numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_01_150811) do
+ActiveRecord::Schema.define(version: 2021_05_01_151911) do
 
   create_table "flats", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_05_01_150811) do
     t.integer "number_of_guests"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "picture_url"
   end
 
 end
